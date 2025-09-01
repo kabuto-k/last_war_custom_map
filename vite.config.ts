a57bbbe8197f43e6a31d113c85b1a721f18539c4
kabuto-k <kabuto0805@gmail.com>
2025-09-02 01:51:05 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/last_war_custom_map/', // この行を追加
+      plugins: [react()],
+      base: '/last_war_custom_map/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
